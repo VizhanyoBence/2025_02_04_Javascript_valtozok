37b5e0b78ee42067b44c58ff7166deb992cd61b9
VizhanyoBence <vizhanyo.bence@diak.szbi-pg.hu>
2025-02-04 10:32:55 +0100

    Valtozok gyakorlása és azok példája

---
+++ 01_valtozok.js
@@ -0,0 +1,17 @@
+console.log("Hello world!")
+// változók deklarálása.
+var a = 2; 
+console.log(a); 
+// változók értékének módosítása
+let b = 3; 
+b = 4; 
+console.log(b)
+const c = 5; 
+console.log(c)
+//Var változó újrafiniálása
+var x = 5; 
+console.log(x); 
+var x = "alma"; 
+console.log(x); 
+let d = 3; 
+console.log(d)
---
+++ 02_valtozo_tipusok.js
@@ -0,0 +1,41 @@
+let e = 2;
+console.log('e=' + e + 'típusa:' + typeof e)
+
+const pi = 3.14
+console.log('pi=' + pi + 'típusa:' + typeof pi)
+
+console.log(0.1+0.2);
+console.log((0.1+0.2).toFixed(2));
+
+console.log(1/0);
+console.log(-1/0);
+console.log(0/0);
+
+let firstname = 'John'
+let lastname = 'Doe'
+// Template literals (Template strings)
+// backticks (`)
+console.log(`Hello, ${firstname} ${lastname}! `)
+
+let person = {
+    name: 'Eminem'
+    age: 39
+    isStudent: false
+}
+console.log(person);
+console.log(person.name);
+
+console.log(person);
+console.log(person.name);
+console.log(person.age);
+console.log(person.isStudent);
+console.log(person.address);
+
+let xy;
+console.log('\nBOOLEAN');
+console.log(typeof);
+console.log(Boolean(0));
+console.log(Boolean(""));
+console.log(Boolean(""));
+console.log(Boolean(a));
+console.log(Boolean(1));
